acba6797409d4fe248c1b8a8aab74fdf060126d6
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-03-16 17:55:41 -0400

    Solution for excercise S04.02

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +16,14 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        TextView dataTextView = (TextView) findViewById(R.id.tv_data);
+
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent parentIntent = getIntent();
+        String dataReceived = parentIntent.getStringExtra(Intent.EXTRA_TEXT);
+        Log.d(DetailActivity.class.getSimpleName(), "Data received: "+ dataReceived);
+
+        dataTextView.setText(dataReceived);
+
     }
 }
